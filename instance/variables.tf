98f81cd25d1876f093bec1c90bbfc9a8d2350e3e
Caglar Cavdar <chaglare@gmail.com>
2019-08-13 19:54:47 -0500

    edited

---
+++ instance/variables.tf
@@ -0,0 +1,24 @@
+variable "instance_name" {
+  default = "bastion-tf"
+}
+
+variable "count" {
+  default = "1"
+}
+
+variable "project" {
+  default = "k8-cluster3"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
+variable "machine_type" {
+    default = "f1-micro"
+}
+
